06e7bb3fcba78efb40110c66ed87e88d217ad2cb
Newmindr Deploy <deploy@newmindr.com>
2026-01-04 16:34:06 +0200

    Change email sender from inbox@newmindr.com to no-reply@newmindr.com

--- src/utils/emailConfig.ts
+++ src/utils/emailConfig.ts
@@ -1,6 +1,6 @@
 export const EMAIL_CONFIG = {
     RESEND_API_KEY: 're_Hy7ja2GF_ADaDnFERb3hCSf9kd5YhUpP3',
-    FROM_EMAIL: 'newmindr <inbox@newmindr.com>',
+    FROM_EMAIL: 'newmindr <no-reply@newmindr.com>',
     SUPABASE_URL: 'https://ueobnqseskhrpezrvukk.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlb2JucXNlc2tocnBlenJ2dWtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcyODY4MjYsImV4cCI6MjA4Mjg2MjgyNn0.k_T7FLtqYHh_pHstM4P5OzUEfUyFcs0XfF8sUFiwSM',
 };
